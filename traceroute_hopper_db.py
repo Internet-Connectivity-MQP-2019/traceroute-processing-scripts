8a5792508cf0787f34cf181e38a910f022091744
Sourec <tothemyers@gmail.com>
2019-09-03 21:16:10 -0400

    Switch to using bulk inserts

--- traceroute_hopper_db.py
+++ traceroute_hopper_db.py
@@ -37,7 +37,7 @@ connection.setencoding(encoding="utf-8")
 connection.maxwrite = 2 << 32
 
 cursor = connection.cursor()
-insert_stmt = "INSERT INTO hops (src, dst, rtt, src_lat, src_lng, dst_lat, dst_lng) VALUES (?, ?, ?, ?, ?, ?, ?);"
+hops = []
 with open(sys.argv[1], "r") as file:
 	for i, line in enumerate(file):
 		traceroute = json.loads(line)
@@ -47,9 +47,11 @@ with open(sys.argv[1], "r") as file:
 			base_src = str(socket.gethostbyname(traceroute["hostname"]))
 			continue
 
-		if i % 5000 == 0:
+		if i % 10000 == 0 and len(hops) != 0:
 			print("Processed {} traceroutes".format(i))
-			connection.commit()
+			args_str = ",".join(("('%s', '%s', %d, %s, %s, %s, %s)" % tuple(hop)) for hop in hops)
+			cursor.execute("INSERT INTO hops VALUES " + args_str)
+			hops.clear()
 
 		if "hops" not in traceroute.keys():
 			# Some traceroutes fail but the rest of the file should still be okay
@@ -62,30 +64,33 @@ with open(sys.argv[1], "r") as file:
 			rtt = hop["rtt"] if j == 0 else hop["rtt"] - traceroute["hops"][j - 1]["rtt"]
 
 			# Get IP locations, first by trying the cache, then by trying the locations db
-			coords = {src: {"lat": None, "lng": None}, dst: {"lat": None, "lng": None}}
+			coords = {src: {"lat": "NULL", "lng": "NULL"}, dst: {"lat": "NULL", "lng": "NULL"}}
 			for ip in coords.keys():
 				if ip not in locations_cache.keys():
 					try:
 						response = locations_db.city(ip)
 						locations_cache[ip] = {
-							"lat": response.location.latitude,
-							"lng": response.location.longitude,
+							"lat": response.location.latitude if response.location.latitude is not None else "NULL",
+							"lng": response.location.longitude if response.location.longitude is not None else "NULL"
 						}
 					except (geoip2.errors.AddressNotFoundError, ValueError):
-						locations_cache[ip] = {"lat": None, "lng": None}
+						locations_cache[ip] = {"lat": "NULL", "lng": "NULL"}
 				coords[ip] = locations_cache[ip]
 
 			# Insert hop data into database
-			cursor.execute(insert_stmt, (src, dst, rtt, coords[src]["lat"], coords[src]["lng"], coords[dst]["lat"],
-										 coords[dst]["lng"]))
+			hops.append([src, dst, rtt, coords[src]["lat"], coords[src]["lng"], coords[dst]["lat"],
+										 coords[dst]["lng"]])
 
 			# Consider the RTT for the source to the destination too, not just the hop
 			if j != 0:
-				cursor.execute(insert_stmt, (base_src, dst, hop["rtt"], locations_cache[base_src]["lat"],
-								locations_cache[base_src]["lng"], coords[dst]["lat"], coords[dst]["lng"]))
+				hops.append([base_src, dst, hop["rtt"], locations_cache[base_src]["lat"],
+								locations_cache[base_src]["lng"], coords[dst]["lat"], coords[dst]["lng"]])
 
-print("Saving to database...")
+print("Committing to database...")
+if len(hops) != 0:
+	args_str = ",".join(("('%s', '%s', %d, %s, %s, %s, %s)" % tuple(hop)) for hop in hops)
+	cursor.execute("INSERT INTO hops VALUES " + args_str)
 cursor.commit()
-print("Saved!")
+print("Committed!")
 connection.close()
 locations_db.close()
