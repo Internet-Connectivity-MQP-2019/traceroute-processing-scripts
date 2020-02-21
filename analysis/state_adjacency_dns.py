1d32b7e3a0fdd845bcd04e06b52d4fdeb6971da1
Sourec <tothemyers@gmail.com>
2020-02-20 21:53:04 -0500

    Update for corrected DNS stuff and ordering whatnot [i'm tired]

--- analysis/state_adjacency_dns.py
+++ analysis/state_adjacency_dns.py
@@ -1,10 +1,24 @@
 import networkx as nx
 import pandas as pd
 
-df: pd.DataFrame = pd.read_csv("rtt_3_0.2_kruskals_p.csv", index_col=0)
+df: pd.DataFrame = pd.read_csv("dns_rtt_normalized_p_values_2_1.0.csv", index_col=0)
+# df: pd.DataFrame = pd.read_csv("rtt_3_0.2_kruskals_p.csv", index_col=0)
 graph = nx.convert_matrix.from_pandas_adjacency(df)
 df = nx.convert_matrix.to_pandas_edgelist(graph)
 df = df[df["source"] != df["target"]]
 df.rename({"source": "state1", "target": "state2", "weight": "p"}, inplace=True, axis=1)
-df.sort_values(["p"], ascending=False, inplace=True)
-df.to_pickle("state_adjacency_dns.pkl")
+df = df[df.p < 0.05]
+
+df_medians: pd.DataFrame = pd.read_pickle("dns_rtt_normalized_2_1.0_pairs_pickle.pkl")
+df_medians.index = df_medians.index.droplevel([2, 3])
+df_medians = df_medians.groupby(level=0).median()
+
+df = df.join(df_medians, "state1").join(df_medians, "state2", rsuffix="2")
+comp = df["median"] < df["median2"]
+df.loc[comp, ["state1", "state2"]] = df.loc[comp, ["state2", "state1"]].values
+df.loc[comp, ["median", "median2"]] = df.loc[comp, ["median2", "median"]].values
+df["ratio"] = df["median2"] / df["median"]
+df.drop(["median", "median2"], inplace=True, axis=1)
+
+df.to_pickle("state_adjacency_dns2_norm.pkl")
+
--- analysis/states_better_than.py
+++ analysis/states_better_than.py
@@ -1,17 +1,22 @@
 import pandas as pd
 
-datasets = ["caida", "dns", "siteping"]
+datasets = ["caida", "siteping", "dns2", "dns2_norm"]
 dfs = {}
 for dataset in datasets:
 	df: pd.DataFrame = pd.read_pickle("state_adjacency_{}.pkl".format(dataset))
-	df.drop(["state1", "h"], inplace=True, axis=1, errors="ignore")
+	df.drop(["state1", "h", "p"], inplace=True, axis=1, errors="ignore")
 	df.rename({"state2": "state"}, inplace=True, axis=1)
 	g = df.groupby("state")
-	cols = {col: ["median", "mean"] for col in df.columns if col != "state"}
-	cols["state"] = "count"
-	df = g.agg(cols)
+	# Allows processing of ratio too, just remove ratio from list of dropped columns
+	if len(df.columns) > 1:
+		cols = {col: ["median", "mean"] for col in df.columns if col != "state"}
+		cols["state"] = "count"
+		df = g.agg(cols)
+	else:
+		df = pd.DataFrame({("state", "count"): g.state.count()})
 	dfs[dataset] = df
 
+
+# Put everything together
 df = pd.concat(dfs, axis=1, sort=True)
 cr = df.corr()
-
